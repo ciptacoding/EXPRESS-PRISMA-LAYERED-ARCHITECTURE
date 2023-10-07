b4b7c8b20b6b5f58473858b66449c7fd09928479
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-07 21:37:34 +0800

    Feat: deleted product by id

--- src/index.js
+++ src/index.js
@@ -8,18 +8,21 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
+// express middleware to read json
 app.use(express.json());
 
 app.get("/", (req, res) => {
    res.send("Hello Sepuh puh");
 });
 
+// get all data products
 app.get("/products", async (req, res) => {
    const products = await prisma.product.findMany();
 
    res.send(products);
 });
 
+// store new data product
 app.post("/products", async (req, res) => {
    const newProductData = req.body;
 
@@ -38,6 +41,20 @@ app.post("/products", async (req, res) => {
    });
 });
 
+// delete product by id
+app.delete("/products/:id", async (req, res) => {
+   const productId = req.params.id; //id in url is string
+
+   await prisma.product.delete({
+      where: {
+         id: parseInt(productId),
+      },
+   });
+
+   res.send("Product Deleted");
+});
+
+// port running
 app.listen(PORT, () => {
    console.log("Express API running in port:" + PORT);
 });
