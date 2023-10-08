67ab2f9c332c0e2d66104f270b0604c1cee401e1
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-08 13:23:54 +0800

    Feat: added method PUT

--- src/index.js
+++ src/index.js
@@ -54,6 +54,40 @@ app.delete("/products/:id", async (req, res) => {
    res.send("Product Deleted");
 });
 
+// update using method PUT
+app.put("/products/:id", async (req, res) => {
+   const productId = req.params.id;
+   const productData = req.body;
+
+   if (
+      !(
+         productData.name &&
+         productData.description &&
+         productData.price &&
+         productData.image
+      )
+   ) {
+      return res.status(400).send("Some field are missing");
+   }
+
+   const product = await prisma.product.update({
+      where: {
+         id: parseInt(productId),
+      },
+      data: {
+         name: productData.name,
+         description: productData.description,
+         price: productData.price,
+         image: productData.image,
+      },
+   });
+
+   res.send({
+      data: product,
+      message: "Product has been updated",
+   });
+});
+
 // port running
 app.listen(PORT, () => {
    console.log("Express API running in port:" + PORT);
